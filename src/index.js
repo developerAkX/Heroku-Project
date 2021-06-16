9313fef119af46c7b1f6990b3bed69d9467db4dd
Ayushks11560 <54079897+Ayushks11560@users.noreply.github.com>
2021-06-16 18:41:28 +0530

    Weather App v1.5

--- src/index.js
+++ src/index.js
@@ -2,57 +2,69 @@ const requests = require("requests");
 const express = require("express");
 const path = require("path");
 const hbs = require("hbs");
+const axios = require('axios');
 
 const app = express();
 const port = process.env.PORT || 8000;
+const apiKey = `51ddb5900aa0c587290765cefd31ff38`;
+
 
 // Defining Paths
 const staticPath = path.join(__dirname, "../public");
 const viewsPath = path.join(__dirname, "../layout/views");
 const partialsPath = path.join(__dirname, "../layout/components");
 
+
 // Setting-up app
 app.set("view engine", "hbs");
 app.set("views", viewsPath);
 hbs.registerPartials(partialsPath);
 
+
 // Setting-up middleware
 app.use(express.static(staticPath));
 
+
 // Setting-up routes
 app.get("/", (req, res) => {
-  if (req.query.city) {
-    requests(
-      `http://api.openweathermap.org/data/2.5/weather?q=${req.query.city}&appid=51ddb5900aa0c587290765cefd31ff38`
-    )
-      // On data
-      .on("data", (chunk) => {
-        const data = JSON.parse(chunk);
-        console.log(data);
-
-        res.render("index", {
-          tempVal: `${Math.round(data.main.temp - 273.15)} °C`,
-          tempMin: `${Math.round(data.main.temp_min - 273.15)} °C`,
-          tempMax: `${Math.round(data.main.temp_max - 273.15)} °C`,
-          location: data.name,
-          country: data.sys.country,
-          tempStatus: data.weather[0].main,
-        });
+  const getWeatherReport = async (city) => {
+    try {
+      const response = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=51ddb5900aa0c587290765cefd31ff38`);
+      const WeatherReport = response.data;
+      res.render("index", {
+        tempVal: `${Math.round(WeatherReport.main.temp - 273.15)} °C`,
+        tempMin: `${Math.round(WeatherReport.main.temp_min - 273.15)} °C`,
+        tempMax: `${Math.round(WeatherReport.main.temp_max - 273.15)} °C`,
+        location: WeatherReport.name,
+        country: WeatherReport.sys.country,
+        tempStatus: WeatherReport.weather[0].main,
       })
-      // On end
-      .on("end", (err) => {
-        err ? console.log("connection closed due to errors", err) : null;
-      });
-  } else {
-    res.render("start");
+    } catch (error) {
+      console.log(404, "render");
+      res.render("404start", {
+        city: city
+      })
+      console.log(error);
+      return error;
+    }
+  }
+  if (req.query.city)
+    getWeatherReport(req.query.city);
+  else {
+    console.log(404, "render");
+    res.render("404start", { city: req.query.city })
   }
 });
+
 
 app.get('*', (req, res) => {
   res.render("404", {
     error: "Page not found"
   });
 });
+
+
+// listening server
 app.listen(port, () => {
-  console.log(`Server started on http://localhost:8000/?city=hajipur`);
+  console.log(`Server started on ${port}`);
 });
